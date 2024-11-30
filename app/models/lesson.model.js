f5b23b169a97b581f5254c3a5aabccfeaeff9fbe
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-11-30 19:21:35 +0300

    update lesson model

--- app/models/lesson.model.js
+++ app/models/lesson.model.js
@@ -3,7 +3,7 @@ import { Schema,model } from "mongoose";
 const lessonSchema = new Schema({
     name: {
         type: String,
-        enum: ['Primary', 'Secondary', 'College'],
+        enum: ['ECDE','Primary', 'Secondary', 'College',"University"],
         required: true,
     },
     description: { type: String, required: true },
